10f9eb70b2e466b831e8407fe7c7014e1c333baa
zhangshengjie <z.cejay@gmail.com>
2022-09-09 17:03:28 +0800

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,53 @@
  * @Autor: z.cejay@gmail.com
  * @Date: 2022-08-04 21:05:35
  * @LastEditors: cejay
- * @LastEditTime: 2022-09-08 22:27:40
+ * @LastEditTime: 2022-09-09 17:02:44
  */
 
 import { assert } from 'console';
+import { ethers } from 'ethers';
 import Web3 from 'web3';
 import { Utils } from './utils';
+import { ecsign, toRpcSig, keccak256 } from 'ethereumjs-util'
+import { arrayify } from 'ethers/lib/utils';
 
 async function main() {
     const web3 = new Web3('http://localhost:8545');
+
+
+    {
+        // 签名
+        /*
+        function dividend(
+            uint256 amount_,
+            uint256 nonce_,
+            bytes memory signature_
+        ) external pure returns (address) {
+            address sender = 0xB5eF866Aa826E1428f38b0C9396F8348167e44fD;
+            bytes32 message = keccak256(abi.encodePacked(nonce_, sender, amount_));
+    
+            return message.toEthSignedMessageHash().recover(signature_);
+        }
+         */
+        //bytes32 message = keccak256(abi.encodePacked(nonce_, sender, amount_));
+        const nonce = 1;
+        const sender = '0xB5eF866Aa826E1428f38b0C9396F8348167e44fD';
+        const amount = 100;
+        //abi.encodePacked(nonce_, sender, amount_)
+        let message = ethers.utils.solidityPack(['uint256', 'address', 'uint256'], [nonce, sender.toLowerCase(), amount]);
+        // keccak256(message)
+        message = ethers.utils.keccak256(message);
+        let privateKey: string = '0x7f0ef6c943faa45cf9bc338bc051821e78201870c034464499bb9024f3f7e27e';
+        const msg1 = Buffer.concat([
+            Buffer.from('\x19Ethereum Signed Message:\n32', 'ascii'),
+            Buffer.from(arrayify(message))
+        ])
+        const sig = ecsign(keccak256(msg1), Buffer.from(arrayify(privateKey)))
+        const signedMessage = toRpcSig(sig.v, sig.r, sig.s);
+        console.log('signedMessage', signedMessage);
+    }
+
+
     await Utils.sleep(1000 * 1);
     const accounts = await web3.eth.getAccounts();
     // each accounts
